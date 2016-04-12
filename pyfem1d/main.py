db2788de63cadf48007bfe9fb5728f3f8282bd61
onursolmaz <onursolmaz@gmail.com>
2016-04-12 23:53:51 +0200

    Minor

---
+++ pyfem1d/main.py
@@ -0,0 +1,31 @@
+# from __future__ import division, print_function, absolute_import
+
+import pyfem1d.tkgui
+import pyfem1d.analysis
+import Tkinter
+from multiprocessing import Process
+
+def __main__():
+    an = pyfem1d.analysis.Analysis()
+    an.execution.parseCmd()
+
+    if not an.execution.silent:
+        an.execution.printHeader()
+
+    # Execute the input file if given
+    if an.execution.inputFile:
+        an.cmdShell.execFile(an.execution.inputFile)
+        #execfile(an.execution.inputFile)
+
+    # Start gui if -g was used
+    if an.execution.gui:
+        pyfem1d.tkgui.startGui(an)
+
+    # Start interactive shell if -i was given or
+    # there was neither input file nor -g
+    if an.execution.interactive or (not an.execution.inputFile and not an.execution.gui):
+        an.startShell()
+
+
+if __name__ == "__main__":
+    __main__()
