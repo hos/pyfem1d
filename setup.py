46903d99704912ea8dca3e676bc59e88a26cb4ec
onursolmaz <onursolmaz@gmail.com>
2016-04-12 23:49:17 +0200

    Minor

--- setup.py
+++ setup.py
@@ -1,15 +1,40 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
 
-setup(name = "pyfem1d",
-    version = "100",
-    description = "1d finite elements for testing material formulations",
-    author = "nrs",
-    author_email = "onur@kreix.com",
-    url = "https://github.com/nrs",
-    packages = ['pyfem1d', 'pyfem1d.constitutive', 'pyfem1d.load'],
-    package_data = {'pyfem1d' : ["pyfem1d/*.py"],
-                    'pyfem1d.constitutive' : ["pyfem1d/constitutive/*.py"],
-                    'pyfem1d.load' : ["pyfem1d/load/*.py"]},
-    scripts = ["bin/pyfem1d"],
-    #long_description = """Really long text here."""
+here = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name="pyfem1d",
+
+    version="0.0",
+
+    description="1d finite elements for testing material formulations",
+
+    author="H. Onur Solmaz",
+
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+
+    extras_require={
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    },
+
+    # package_data={
+    #     "sample": ["package_data.dat"],
+    # },
+
+    # data_files=[("my_data")],
+
+    install_requires={
+    },
+
+    entry_points={
+        "console_scripts": [
+            "pyfem1d=pyfem1d.main:__main__",
+        ],
+    },
 )
+
+
+
