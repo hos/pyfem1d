67cfa25ba1102b68c82586be3b7e1183272c184e
onursolmaz <onursolmaz@gmail.com>
2016-04-28 23:30:29 +0200

    Minor

--- setup.py
+++ setup.py
@@ -7,13 +7,17 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name="pyfem1d",
 
-    version="0.0",
+    version="0.0.1",
 
     description="1d finite elements for testing material formulations",
 
     author="H. Onur Solmaz",
 
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
+
+    download_url = "https://github.com/hos/pyfem1d",
+
+    keywords = ["FEM", "materials", "mechanics"],
 
     extras_require={
         "dev": ["check-manifest"],
